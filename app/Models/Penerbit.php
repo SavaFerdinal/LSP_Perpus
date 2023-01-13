5f75271cdf252a2f73f559601f719195a2099e30
SavaFerdinal <sava.ferdinal@gmail.com>
2023-01-13 16:34:37 +0700

    Update All API Done

--- app/Models/Penerbit.php
+++ app/Models/Penerbit.php
@@ -9,6 +9,12 @@ class Penerbit extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'kode',
+        'nama',
+        'verif'
+    ];
+
     public function bukus() {
         return $this->hasMany(Buku::class);
     }
